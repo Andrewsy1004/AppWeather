7a714848b3de043732bcd29fb1eead2943f5a27b
Andrewsy1004 <136858623+Andrewsy1004@users.noreply.github.com>
2023-11-28 03:13:50 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
     } else {
       try {
         const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city},${pais}&appid=${process.env.appid}`
+          `https://api.openweathermap.org/data/2.5/weather?q=${city},${pais}&appid=${process.env.REACT_APP_APPID}`
         );
 
         setWeatherData(response.data);
